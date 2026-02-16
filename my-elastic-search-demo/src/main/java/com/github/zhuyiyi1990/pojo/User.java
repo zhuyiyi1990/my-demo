8f748b9435590da411c84a753a82fd12b2cf9d34
朱一一 <zhuyiyi1990@outlook.com>
2026-02-16 10:44:37 +0800

    demo

---
+++ my-elastic-search-demo/src/main/java/com/github/zhuyiyi1990/pojo/User.java
@@ -0,0 +1,14 @@
+package com.github.zhuyiyi1990.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+
+    private String sex;
+
+    private Integer age;
+
+}
--- my-elastic-search-demo/src/test/java/com/github/zhuyiyi1990/MyElasticSearchDemoApplicationTests.java
+++ my-elastic-search-demo/src/test/java/com/github/zhuyiyi1990/MyElasticSearchDemoApplicationTests.java
@@ -1,17 +1,34 @@
 package com.github.zhuyiyi1990;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.zhuyiyi1990.pojo.User;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+import org.elasticsearch.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -60,7 +77,6 @@ class MyElasticSearchDemoApplicationTests {
     void contextLoads() {
     }
 
-    // 创建索引
     @Test
     void testCreateIndex() throws Exception {
         // 创建索引
@@ -70,6 +86,109 @@ class MyElasticSearchDemoApplicationTests {
         // 响应状态
         boolean acknowledged = createIndexResponse.isAcknowledged();
         System.out.println("索引操作 ：" + acknowledged);
+    }
+
+    @Test
+    void testGetIndex() throws Exception {
+        // 查询索引
+        GetIndexRequest request = new GetIndexRequest("user");
+
+        GetIndexResponse getIndexResponse = esClient.indices().get(request, RequestOptions.DEFAULT);
+
+        System.out.println(getIndexResponse.getAliases());
+        System.out.println(getIndexResponse.getMappings());
+        System.out.println(getIndexResponse.getSettings());
+    }
+
+    @Test
+    void testDeleteIndex() throws Exception {
+        // 删除索引
+        DeleteIndexRequest request = new DeleteIndexRequest("user");
+
+        AcknowledgedResponse response = esClient.indices().delete(request, RequestOptions.DEFAULT);
+
+        // 响应状态
+        System.out.println(response.isAcknowledged());
+    }
+
+    @Test
+    void testInsertDoc() throws Exception {
+        // 插入数据
+        IndexRequest request = new IndexRequest();
+        request.index("user").id("1001");
+
+        User user = new User();
+        user.setName("zhangsan");
+        user.setAge(30);
+        user.setSex("男");
+
+        // 向ES插入数据，必须将数据转换位JSON格式
+        ObjectMapper mapper = new ObjectMapper();
+        String userJson = mapper.writeValueAsString(user);
+        request.source(userJson, XContentType.JSON);
+
+        IndexResponse response = esClient.index(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.getResult());
+    }
+
+    @Test
+    void testUpdateDoc() throws Exception {
+        // 修改数据
+        UpdateRequest request = new UpdateRequest();
+        request.index("user").id("1001");
+        request.doc(XContentType.JSON, "sex", "女");
+
+        UpdateResponse response = esClient.update(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.getResult());
+    }
+
+    @Test
+    void testGetDoc() throws Exception {
+        // 查询数据
+        GetRequest request = new GetRequest();
+        request.index("user").id("1001");
+        GetResponse response = esClient.get(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.getSourceAsString());
+    }
+
+    @Test
+    void testDeleteDoc() throws Exception {
+        DeleteRequest request = new DeleteRequest();
+        request.index("user").id("1001");
+
+        DeleteResponse response = esClient.delete(request, RequestOptions.DEFAULT);
+        System.out.println(response.toString());
+    }
+
+    @Test
+    void testInsertDocBatch() throws Exception {
+        // 批量插入数据
+        BulkRequest request = new BulkRequest();
+
+        request.add(new IndexRequest().index("user").id("1001").source(XContentType.JSON, "name", "zhangsan", "age", 30, "sex", "男"));
+        request.add(new IndexRequest().index("user").id("1002").source(XContentType.JSON, "name", "lisi", "age", 30, "sex", "女"));
+        request.add(new IndexRequest().index("user").id("1003").source(XContentType.JSON, "name", "wangwu", "age", 40, "sex", "男"));
+
+        BulkResponse response = esClient.bulk(request, RequestOptions.DEFAULT);
+        System.out.println(response.getTook());
+        System.out.println(response.getItems());
+    }
+
+    @Test
+    void testDeleteDocBatch() throws Exception {
+        // 批量删除数据
+        BulkRequest request = new BulkRequest();
+
+        request.add(new DeleteRequest().index("user").id("1001"));
+        request.add(new DeleteRequest().index("user").id("1002"));
+        request.add(new DeleteRequest().index("user").id("1003"));
+
+        BulkResponse response = esClient.bulk(request, RequestOptions.DEFAULT);
+        System.out.println(response.getTook());
+        System.out.println(response.getItems());
     }
 
 }
