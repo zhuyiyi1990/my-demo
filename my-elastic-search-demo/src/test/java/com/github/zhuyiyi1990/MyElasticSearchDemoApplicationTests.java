8bf234066e45a26e83e1659fc50a12eb8d75cf78
朱一一 <zhuyiyi1990@outlook.com>
2026-02-18 16:16:10 +0800

    demo

--- my-elastic-search-demo/src/test/java/com/github/zhuyiyi1990/MyElasticSearchDemoApplicationTests.java
+++ my-elastic-search-demo/src/test/java/com/github/zhuyiyi1990/MyElasticSearchDemoApplicationTests.java
@@ -1,6 +1,7 @@
 package com.github.zhuyiyi1990;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.zhuyiyi1990.dao.ProductDAO;
 import com.github.zhuyiyi1990.pojo.Product;
 import com.github.zhuyiyi1990.pojo.User;
 import org.apache.http.HttpHost;
@@ -49,8 +50,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @SpringBootTest
@@ -60,6 +66,9 @@ class MyElasticSearchDemoApplicationTests {
 
     @Autowired
     private ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    @Autowired
+    private ProductDAO productDAO;
 
     @BeforeEach
     void connect() {
@@ -493,16 +502,98 @@ class MyElasticSearchDemoApplicationTests {
     }
 
     @Test
-    public void testCreateIndexUsingSpringBoot() {
+    void testCreateIndexUsingSpringBoot() {
         // 创建索引，系统初始化会自动创建索引
         System.out.println("创建索引");
     }
 
     @Test
-    public void testDeleteIndexUsingSpringBoot() {
+    void testDeleteIndexUsingSpringBoot() {
         // 删除索引
         boolean flg = elasticsearchRestTemplate.indexOps(Product.class).delete();
         System.out.println("删除索引 = " + flg);
+    }
+
+    // 新增
+    @Test
+    void testSave() {
+        Product product = new Product();
+        product.setId(2L);
+        product.setTitle("华为手机");
+        product.setCategory("手机");
+        product.setPrice(2999.0);
+        product.setImages("http://www.atguigu.com/hw.jpg");
+        productDAO.save(product);
+    }
+
+    // 修改
+    @Test
+    void testUpdate() {
+        Product product = new Product();
+        product.setId(2L);
+        product.setTitle("小米2手机");
+        product.setCategory("手机");
+        product.setPrice(9999.0);
+        product.setImages("http://www.atguigu.com/xm.jpg");
+        productDAO.save(product);
+    }
+
+    // 根据id查询
+    @Test
+    void testFindById() {
+        Product product = productDAO.findById(2L).get();
+        System.out.println(product);
+    }
+
+    // 查询所有
+    @Test
+    void testFindAll() {
+        Iterable<Product> products = productDAO.findAll();
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+
+    // 删除
+    @Test
+    void testDelete() {
+        Product product = new Product();
+        product.setId(2L);
+        productDAO.delete(product);
+    }
+
+    // 批量新增
+    @Test
+    void testSaveAll() {
+        List<Product> productList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Product product = new Product();
+            product.setId((long) i);
+            product.setTitle("[" + i + "]小米手机");
+            product.setCategory("手机");
+            product.setPrice(1999.0 + i);
+            product.setImages("http://www.atguigu.com/xm.jpg");
+            productList.add(product);
+        }
+        productDAO.saveAll(productList);
+    }
+
+    // 分页查询
+    @Test
+    void testFindByPageable() {
+        // 设置排序(排序方式，正序还是倒序，排序的字段)
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        // 当前页，第一页从0开始，1表示第二页
+        int currentPage = 0;
+        // 每页显示多少条
+        int pageSize = 5;
+        // 设置查询分页
+        PageRequest pageRequest = PageRequest.of(currentPage, pageSize, sort);
+        // 分页查询
+        Page<Product> productPage = productDAO.findAll(pageRequest);
+        for (Product product : productPage.getContent()) {
+            System.out.println(product);
+        }
     }
 
 }
