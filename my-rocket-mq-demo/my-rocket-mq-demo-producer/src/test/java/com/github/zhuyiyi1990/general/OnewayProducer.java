cd0670bc41f72bfe712b0e06c3a1571814b7ad61
朱一一 <zhuyiyi1990@outlook.com>
2026-05-03 10:57:23 +0800

    demo

---
+++ my-rocket-mq-demo/my-rocket-mq-demo-producer/src/test/java/com/github/zhuyiyi1990/general/OnewayProducer.java
@@ -0,0 +1,23 @@
+package com.github.zhuyiyi1990.general;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+
+public class OnewayProducer {
+
+    public static void main(String[] args) throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("pg");
+        producer.setNamesrvAddr("192.168.58.100:9876");
+        producer.start();
+
+        for (int i = 0; i < 10; i++) {
+            byte[] body = ("Hi," + i).getBytes();
+            Message msg = new Message("single", "someTag", body);
+            // 单向发送
+            producer.sendOneway(msg);
+        }
+        producer.shutdown();
+        System.out.println("producer shutdown");
+    }
+
+}
